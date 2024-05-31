836324365a1b0e51fed7e46d99401e3aa50cb0d9
Aditi-R-Vadi <vadiaditi@gmail.com>
2024-06-01 01:24:41 +0530

    just

--- app/comp/HeroSection.js
+++ app/comp/HeroSection.js
@@ -59,8 +59,9 @@ const HeroSection = () => {
               href="/"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
+
               <button onClick={handleViewPDF}
-              className="block bg-[#121212] hover:bg-slate-800 rounded-full px-4 py-2 sm:px-5 sm:py-2 md:px-6 md:py-3 lg:px-8 lg:py-4 text-xs sm:text-sm md:text-base lg:text-lg">
+              className="block bg-[#121212] sm:w-fit hover:bg-slate-800 rounded-full px-4 py-2 sm:px-5 sm:py-2 md:px-6 md:py-3 lg:px-8 lg:py-4 text-xs sm:text-sm md:text-base lg:text-lg">
                 Download Resume
               </button>
             </Link>
