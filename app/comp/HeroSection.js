e21c3b8d3e0fb85d7bc2f3c281df55383ef8633e
Aditi-R-Vadi <133814622+AditiVadi@users.noreply.github.com>
2025-08-05 23:02:11 +0530

    Update HeroSection.js

--- app/comp/HeroSection.js
+++ app/comp/HeroSection.js
@@ -55,16 +55,16 @@ const HeroSection = () => {
             >
               Hire Me
             </Link>
-            <Link
-              href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-            >
+            // <Link
+            //   href="/"
+            //   className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            // >
 
-              <button onClick={handleViewPDF}
-              className="block bg-[#121212] hover:bg-slate-800 rounded-full px-4 py-2 sm:px-5 sm:py-3 md:px-6 md:py-3 lg:px-8 lg:py-4 text-xs sm:text-sm md:text-base lg:text-lg">
-                Download Resume
-              </button>
-            </Link>
+            //   <button onClick={handleViewPDF}
+            //   className="block bg-[#121212] hover:bg-slate-800 rounded-full px-4 py-2 sm:px-5 sm:py-3 md:px-6 md:py-3 lg:px-8 lg:py-4 text-xs sm:text-sm md:text-base lg:text-lg">
+            //     Download Resume
+            //   </button>
+            // </Link>
           </div>
         </motion.div>
         <motion.div
@@ -90,5 +90,6 @@ const HeroSection = () => {
 
 
 export default HeroSection;
+
 
 
